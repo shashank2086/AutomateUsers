9e43dc7875927519c9d62d82df9267e5d6ef1a61
shashank2086 <46296119+shashank2086@users.noreply.github.com>
2020-10-13 14:25:09 +0530

    Update AccountCreation.ps1

--- AccountCreation.ps1
+++ AccountCreation.ps1
@@ -1,5 +1,5 @@
-$users=Get-WmiObject -Class Win32_UserAccount | select Name
-Remove-LocalUser -Name $users.Name
-$password = ConvertTo-SecureString "password" -AsPlainText -Force
-New-LocalUser "Newuser" -Password $password -Fullname "User" -Description "new"
-Add-LocalGroupMember -Group "Administrators" -Member "NewUser"
+$users=Get-WmiObject -Class Win32_UserAccount | select Name #fetching all users and storing in the array
+Remove-LocalUser -Name $users.Name #removing all users in the given array 
+$password = ConvertTo-SecureString "password" -AsPlainText -Force # converting "password" into a SecureString which can be manually changed according the user
+New-LocalUser "Newuser" -Password $password -Fullname "User" -Description "new" # Adding New-User
+Add-LocalGroupMember -Group "Administrators" -Member "NewUser" # Giving Previlages
